66304e0874bb15021bc10bcfc185891216a40593
Jay-Sn <jasonteo376@gmail.com>
2021-01-21 00:49:48 +0800

    Added Community View and Image

---
+++ EatWhatLah/EatWhatLah/CommunityController.swift
@@ -0,0 +1,37 @@
+//
+//  CommunityController.swift
+//  EatWhatLah
+//
+//  Created by Ajaeson on 21/1/21.
+//
+
+import Foundation
+import UIKit
+
+struct CellData {
+    let image:UIImage?
+    let message : String?
+    
+}
+class CommunityController:UITableViewController{
+    var data = [CellData]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        data = [CellData.init(image: Burger, message: <#T##String?#>)]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+}
---
+++ EatWhatLah/EatWhatLah/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  EatWhatLah
+//
+//  Created by Ajaeson on 21/1/21.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self
+    }
+}
