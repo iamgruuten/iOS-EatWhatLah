357fa7da13f9c71d70b9b886c9ef2aeaf563d61a
Jay-Sn <jasonteo376@gmail.com>
2021-01-28 17:56:15 +0800

    Added ContentController

--- EatWhatLah/EatWhatLah/CommentTableViewCell.swift
+++ EatWhatLah/EatWhatLah/CommentTableViewCell.swift
@@ -11,6 +11,9 @@ class CommentTableViewCell: UITableViewCell {
     @IBOutlet var username:UIButton!
     @IBOutlet var profilePicture:UIButton!
     @IBOutlet var likeButton:UIButton!
+    @IBOutlet var userComment:UITextView!
+    
+    static let identifier = "CommentTableViewCell"
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,5 +25,22 @@ class CommentTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    @IBAction func profilePicture(_ sender: Any) {
+    }
+    @IBAction func username(_ sender: Any) {
+    }
+    @IBAction func likeButton(_ sender: Any) {
+        
+    }
     
+    public func configure(with image:UIImage, name:String, comment:String){
+        profilePicture.setImage(image, for: .normal)
+        username.setTitle(name, for: .normal)
+        userComment.text = comment
+    }
+    
+    static func nib() -> UINib{
+        return UINib(nibName: "CommentTableViewCell", bundle: nil)
+        
+    }
 }
